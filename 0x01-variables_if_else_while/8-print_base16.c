2963db7735b3bc8a25f2897dce5e0fe004e54ad4
elishakuje <elishakuje03@gmail.com>
2022-07-03 20:06:57 -0700

    A program prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
+ * starting from 0, followed by a newline.
  * Return: Always 0 (Success)
  */
 int main(void)
